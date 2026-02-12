e8ab0d4395f3f53fcb747213633db7410b71eef4
yeyongzhi <18258475212m@sina.cn>
2026-02-12 14:15:28 +0800

    build: 📦补充vite路径相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  base: process.env.NODE_ENV === "production" ? "/emoji-collection/" : "./",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
